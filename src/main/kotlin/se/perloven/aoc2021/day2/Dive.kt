684f7a7307d587926841fa2382fe5575357ce366
perloven <per.loven@atg.se>
2021-12-02 15:58:46 +0100

    Day 2 part 2

--- src/main/kotlin/se/perloven/aoc2021/day2/Dive.kt
+++ src/main/kotlin/se/perloven/aoc2021/day2/Dive.kt
@@ -3,13 +3,18 @@ package se.perloven.aoc2021.day2
 import se.perloven.aoc2021.getResourceFile
 
 fun main() {
+    println("Result task 1: ${task1()}")
+    println("Result task 2: ${task2()}")
+}
+
+private fun task1(): Int {
     val file = getResourceFile("day2/dive-input.txt")
     val position = Position()
     file.forEachLine {
         val splitLine = it.split(" ")
         position.modify(splitLine[0], splitLine[1].toInt())
     }
-    println("Result ${position.getResult()}")
+    return position.getResult()
 }
 
 class Position(var horizontalPosition:Int = 0, var depth: Int = 0) {
@@ -18,6 +23,35 @@ class Position(var horizontalPosition:Int = 0, var depth: Int = 0) {
             "forward" -> horizontalPosition += value
             "up" -> depth -= value
             "down" -> depth += value
+        }
+    }
+
+    fun getResult() = horizontalPosition * depth
+}
+
+fun task2(): Int {
+    val file = getResourceFile("day2/dive-input.txt")
+    val position = AimedPosition()
+    file.forEachLine {
+        val splitLine = it.split(" ")
+        position.modify(splitLine[0], splitLine[1].toInt())
+    }
+    return position.getResult()
+}
+
+class AimedPosition(
+    private var horizontalPosition: Int = 0,
+    private var depth: Int = 0,
+    private var aim: Int = 0
+) {
+    fun modify(command: String, value: Int) {
+        when (command) {
+            "forward" -> {
+                horizontalPosition += value
+                depth += aim * value
+            }
+            "up" -> aim -= value
+            "down" -> aim += value
         }
     }
 
